fb2094caeb1967f015ec76a920711c9639f68572
RODRIGUEZ Loïc <contact@rodriguez-loic.me>
2020-08-05 09:46:19 +0100

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    define('START_TIME', microtime(true));
-
-    // For now we only handle the routing
-    // This could be use to load the app configuration, autoload and so on
-    require_once('./routes.php');
