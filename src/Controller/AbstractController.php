4ef9727ab60fe82d425dffa15c65c97ad9de1f80
RODRIGUEZ Loïc <contact@rodriguez-loic.me>
2020-08-06 10:53:18 +0100

    Update AbstractController.php

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -3,73 +3,23 @@
 namespace Controller;
 
 use Core\Template;
+use Core\SessionHandler;
 
 class AbstractController
 {
     protected $template;
 
+    protected $session;
+
     public function __construct($template = '')
     {
         $this->template = new Template($template);
-    }
 
-    protected function request($key) {
-        if (isset($_POST[$key])) {
-            return $_POST[$key];
-        } elseif (isset($_GET[$key])) {
-            return $_GET[$key];
-        } else {
-            return null;
-        }
+        $this->session = new SessionHandler();
     }
 
     protected function getView($controller, array $variables = [])
     {
         return $this->template->getView($controller, $variables);
-    }
-
-    /**
-     * Method used to load views with parameters
-     */
-    protected function loadView(string $viewName = 'index', array $params = [])
-    {
-        extract($params);
-
-        $view_path = DIR_ROOT . 'Views/' . $viewName . '.php';
-
-        if (!file_exists($view_path)) {
-            header('HTTP/1.1 404 Not Found');
-            die('404 - Sorry, it looks like I lost my way for  - ' . $viewName . '(' . $view_path . ')' . ' - view...');
-        }
-
-        require_once($view_path);
-    }
-
-    public static function loadHeader(string $headerName = 'main', array $params = [])
-    {
-        extract($params);
-
-        $view_path = DIR_ROOT . 'Views/headers/' . $headerName . '.php';
-
-        if (!file_exists($view_path)) {
-            header('HTTP/1.1 404 Not Found');
-            die('404 - Sorry, it looks like I lost my way for  - ' . $headerName . '(' . $view_path . ')' . ' - header...');
-        }
-
-        require_once($view_path);
-    }
-
-    public static function loadFooter(string $footerName = 'main', array $params = [])
-    {
-        extract($params);
-
-        $view_path = DIR_ROOT . 'Views/footers/' . $footerName . '.php';
-
-        if (!file_exists($view_path)) {
-            header('HTTP/1.1 404 Not Found');
-            die('404 - Sorry, it looks like I lost my way for  - ' . $footerName . '(' . $view_path . ')' . ' - footer...');
-        }
-
-        require_once($view_path);
     }
 }
